9213b530992d6974ae80e103d3447ebfb02cb564
ishanpillai10 <ishanpillai22@gmail.com>
2025-03-19 00:18:33 +0530

    calback update

--- app.js
+++ app.js
@@ -64,7 +64,7 @@ passport.deserializeUser(async function (id, done) {
 passport.use(new GoogleStrategy({
   clientID: process.env.CLIENT_ID,
   clientSecret: process.env.CLIENT_SECRET,
-  callbackURL: "http://localhost:3000/auth/google/secrets"
+  callbackURL: "https://secrets-iolj.onrender.com/auth/google/secrets"
 },
 async function (accessToken, refreshToken, profile, cb) {
   try {
